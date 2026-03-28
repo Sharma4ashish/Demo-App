5f5333728295969736e49d390f56841dad675594
Ashish Sharma <90973051+Sharma4ashish@users.noreply.github.com>
2026-03-29 04:09:37 +0530

    l

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerController, loginController } = require('../controllers/user');
+const { registerController, loginController, getMe, logoutController } = require('../controllers/user');
+const requireAuth = require('../middlewares/authmiddleware');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
+router.get('/me',requireAuth, getMe);
+router.get('/logout',requireAuth, logoutController);
 
 module.exports = router;
